b923ca496efbfdab50e0cd33c86477733fc83c68
sustrizh <Sergei.Ustrizhitskii@t-systems.com>
2020-08-06 10:37:51 +0300

    Division event offers into different module

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import {createSortTemplate} from "./view/sort";
 import {createEventCardTemplate} from "./view/event/card/event-card";
 import {createEventTypeGroupTemplate} from "./view/event/card/subcomponents/event-type-group";
 import {createEventTypeItemTemplate} from "./view/event/card/subcomponents/event-type-item";
+import {createEventOffersTemplate} from "./view/event/card/subcomponents/event-offers";
 import {createEventListTemplate} from "./view/event/event-list";
 import {createEventItemTemplate} from "./view/event/event-item";
 
@@ -13,6 +14,7 @@ const FILTER_COUNT = 3;
 const EVENT_TYPE_GROUP_COUNT = 2;
 const TRANSFER_EVENT_COUNT = 7;
 const ACTIVITY_EVENT_COUNT = 3;
+const OFFERS_COUNT = 5;
 const EVENT_COUNT = 3;
 
 const render = (container, template, position) => {
@@ -54,6 +56,11 @@ for (let i = 0; i < TRANSFER_EVENT_COUNT; i++) {
 const eventTypeActivityElement = eventTypeListElement.querySelectorAll(`.event__type-group`)[1];
 for (let i = 0; i < ACTIVITY_EVENT_COUNT; i++) {
   render(eventTypeActivityElement, createEventTypeItemTemplate(), `beforeend`);
+}
+
+const offersBlockElement = sortAndContentElement.querySelector(`.event__available-offers`);
+for (let i = 0; i < OFFERS_COUNT; i++) {
+  render(offersBlockElement, createEventOffersTemplate(), `beforeend`);
 }
 
 render(sortAndContentElement, createEventListTemplate(), `beforeend`);
